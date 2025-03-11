5ed94cceabe82b88339ce659425dafe3284d1f47
JLGG2007 <juangouveia070@gmail.com>
2025-03-11 12:17:36 +0000

    feat: create menu admin page and style

---
+++ public/pages/menuadmin.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-PT">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Admin</title>
+    <link rel="stylesheet" href="../styles/global.css">
+    <link rel="stylesheet" href="../styles/css/menuadmin.css">
+  </head>
+  <body>
+    <header class="header">
+      <div class="header-logo">
+        <img src="../images/inovatech_logo.png" alt="Inovatech Logo" class="logo-image">
+      </div>
+      <nav class="header-nav">
+        <div class="header-nav-links">
+          <a href="../index.html" class="nav-link">Início</a>
+          <a href="#" class="nav-link">Conferências</a>
+          <a href="#" class="nav-link">Viagens</a>
+        </div>
+        <div class="header-nav-buttons">
+          <a href="../pages/login.php">
+            <button id="signin">Iniciar Sessão</button>
+          </a>
+          <a href="../pages/register.php">
+            <button id="signup">Criar Conta</button>
+          </a>
+          <a href="../pages/menuadmin.php">
+            <button id="menuadmin">Menu Admin</button>
+          </a>
+          <a href="#">
+            <button id="perfil">Perfil</button>
+          </a>
+        </div>
+      </nav>  
+    </header>
+
+    <main class="menu-admin">
+      <h1>Menu Admin</h1>
+      <div class="buttons">
+        <button>Gerir Conferências</button>
+        <button>Gerir Utilizadores</button>
+        <button>Gerir Viagens</button>
+        <a href="../index.php">
+          <button class="voltar">Voltar</button>
+        </a>
+      </div>
+    </main>
+  </body>
+</html>
